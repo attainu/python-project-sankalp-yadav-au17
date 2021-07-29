a98f35c8791bd45cfe7c5b732e42dc64214b46d1
sankalp-yadav-au17 <sankalp_ma@srmuniv.edu.in>
2021-07-29 22:34:40 +0530

    players class created

---
+++ project/Snake and Ladder/Utilities/players.py
@@ -0,0 +1,28 @@
+import pygame
+from Utilities.screen_size import Size
+
+class Players:
+    def __init__(self):
+        self.font1 = pygame.font.SysFont("comicsansms",30)
+        self.font2 = pygame.font.SysFont("comicsansms",25)
+        self.red=pygame.image.load("Snake and Ladder/Assets/images/red_c.png")
+        self.blue=pygame.image.load("Snake and Ladder/Assets/images/blue_c.png")
+        self.screen = pygame.display.set_mode((Size.width,Size.heigth))
+
+    
+    def red_player(self,rx,ry):
+        msg1 = self.font1.render("Player 1",True,(255,0,0))
+        self.screen.blit(msg1,(250,303))
+        self.screen.blit(self.red,(rx,ry))
+    
+    def blue_player(self,bx,by):
+        msg1 = self.font1.render("Player 2",True,(0,0,255))
+        self.screen.blit(msg1,(250,453))
+        self.screen.blit(self.blue,(bx,by))
+
+    def red_msg(self):
+        msg1=self.font2.render("Your Turn!!!",True,(0,0,0))
+        self.screen.blit(msg1,(255,355))
+    def blue_msg(self):
+        msg1=self.font2.render("Your Turn!!!",True,(0,0,0))
+        self.screen.blit(msg1,(255,504))
--- project/Snake and Ladder/main.py
+++ project/Snake and Ladder/main.py
@@ -8,6 +8,7 @@ from Entities.snake import Snake
 from Entities.ladder import Ladders
 from Entities.dice import Dice
 from Utilities.background import Background
+from Utilities.players import Players
 
 
 pygame.init()
@@ -15,42 +16,10 @@ pygame.init()
 screen = pygame.display.set_mode((Size.width,Size.heigth))
 pygame.display.set_caption("Snake and Ladder Wala Game")
 
-# background
-# snk_lddr_img=pygame.image.load("Snake and Ladder/Assets/images/Snakes_ladders_big_image.png")
-# bckimg=pygame.image.load("Snake and Ladder/Assets/images/introduction_image2.jpg")
-# playbutton = pygame.image.load("Snake and Ladder/Assets/images/playbutton.png")
-# playbutton = pygame.transform.scale(playbutton, (40,40))
-# button = pygame.Rect(50,200,40,40)
+# object for background
+bg = Background()
+plyrs = Players()
 
-# def bck():
-#     screen.blit(bckimg,(0,0))
-#     screen.blit(snk_lddr_img,(433,134))
-#     screen.blit(playbutton,(50,200))
-bg=Background()
-# players
-font1 = pygame.font.SysFont("comicsansms",30)
-font2 = pygame.font.SysFont("comicsansms",25)
-
-red=pygame.image.load("Snake and Ladder/Assets/images/red_c.png")
-blue=pygame.image.load("Snake and Ladder/Assets/images/blue_c.png")
-# player name and dice
-
-def red_player(rx,ry):
-    msg1 = font1.render("Player 1",True,(255,0,0))
-    screen.blit(msg1,(250,303))
-    screen.blit(red,(rx,ry))
-   
-def blue_player(bx,by):
-    msg1 = font1.render("Player 2",True,(0,0,255))
-    screen.blit(msg1,(250,453))
-    screen.blit(blue,(bx,by))
-# player turn message
-def red_msg():
-    msg1=font2.render("Your Turn!!!",True,(0,0,0))
-    screen.blit(msg1,(255,355))
-def blue_msg():
-    msg1=font2.render("Your Turn!!!",True,(0,0,0))
-    screen.blit(msg1,(255,504))
 
     
 # game loop
@@ -69,9 +38,9 @@ def main(turn):
         bg.bck()
 
         if turn == "red":
-            red_msg()
+            plyrs.red_msg()
         elif turn == "blue":
-            blue_msg()
+            plyrs.blue_msg()
             
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -277,19 +246,19 @@ def main(turn):
                 
 
 
-        red_player(rx,ry)
-        blue_player(bx,by)
+        plyrs.red_player(rx,ry)
+        plyrs.blue_player(bx,by)
         pygame.display.update()
         time.sleep(1.3)
 turn = "blue"
 win = main(turn)
 if win == "Red":
-    msg = font1.render("Red successfully crossed the hurdle",True, (255,0,0))
+    msg = plyrs.font1.render("Red successfully crossed the hurdle",True, (255,0,0))
     screen.blit(msg,(409,50))
     pygame.display.update()
     time.sleep (10)
 else:
-    msg = font1.render("Blue successfully crossed the hurdle",True, (255,0,0))
+    msg = plyrs.font1.render("Blue successfully crossed the hurdle",True, (255,0,0))
     screen.blit(msg,(409,50))
     pygame.display.update()
     time.sleep (10)
